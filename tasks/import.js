cd3f966255b8be3b1e3ddd12a0c5001abde266c1
Johnny Hall <johnny@recipher.co.uk>
2016-05-04 10:44:01 +0100

    Stats.com import

--- tasks/import.js
+++ tasks/import.js
@@ -1,7 +1,11 @@
-var Importer = require('../lib/apps/members/services/import');
+var users = require('@ftbl/users')
+  , Importer = require('../lib/apps/members/services/import');
 
-var importer = new Importer;
+var importer = new Importer({ user: users({ name: 'system' }) });
 
 importer.import().then(function() {
-  process.exit(0)
+  process.exit(0);
+}).catch(function(err) {
+  console.log(err);
+  process.exit(1);
 });
