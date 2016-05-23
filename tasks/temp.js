027848d5f1141bce0e2e476d0fac9d0ad4f6fac3
Johnny Hall <johnny@recipher.co.uk>
2016-05-23 11:48:18 +0100

    Fix disconnect

--- tasks/temp.js
+++ tasks/temp.js
@@ -12,7 +12,15 @@ return request('members/members/search', context).get({ limit: 1000, q: '' }).th
 
     return request('members/members/' + member.id + '/connections', context).get().then(function(data) {
       return Promise.map(data.connections, function(connection) {
-        if (ids[connection.id] || connection.member == null) return;
+        var key = [ connection.type, connection.member.id, connection.connectedTo.id ].join(':');
+
+        if (ids[key] || connection.member == null) {
+          var url = 'members/members/' + connection.member.id + '/connection/' + connection.connectedTo.id;
+          console.log(url);
+          return request(url, context).delete({ type: connection.type }).catch(function(err) {
+            console.log(err);
+          });
+        }
 
         var conn = { 
           id: connection.id
@@ -21,9 +29,9 @@ return request('members/members/search', context).get({ limit: 1000, q: '' }).th
         , connectedTo: connection.connectedTo.id 
         };
 
-        ids[conn.id] = conn;
+        ids[key] = conn;
 
-        return sceneskope('member:connection:create', conn, context);
+        // return sceneskope('member:connection:create', conn, context);
       });
     });
   });
